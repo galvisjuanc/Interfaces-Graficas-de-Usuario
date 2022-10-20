ecd145f6d352f23f12fb35702b1130c34296cd9d
Juan <galvisjuanc@gmail.com>
2022-10-20 09:02:16 -0500

    First Window with JFrame

---
+++ src/GUI_2/Ventana.java
@@ -0,0 +1,12 @@
+package GUI_2;
+
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+
+    public Ventana(){
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Mi Primera Ventana en Java");
+    }
+}
