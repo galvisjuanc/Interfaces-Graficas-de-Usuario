06231bcebc78753ef111975e6d358b3e8f2a11d3
Juan <galvisjuanc@gmail.com>
2022-10-27 08:02:25 -0500

    Modifications to Format Letter

--- src/GUI_6/Ventana.java
+++ src/GUI_6/Ventana.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Ventana extends JFrame {
     public Ventana(){
-        setSize(400,400);
+        setSize(600,600);
         setTitle("El mejor titulo");
         // setLocation(100,200);
         // setBounds(100,200, 600, 600);           // Este método engloba ambos metodos setSize, setLocation
@@ -28,15 +28,40 @@ public class Ventana extends JFrame {
 
         panel.setLayout(null);                          // Desactivamos el diseño
         JLabel etiqueta = new JLabel();                 // Creamos etiqueta
+
         etiqueta.setText("Hola");                       // Setear valor
-        etiqueta.setBounds(10, 10,30, 30);
+        etiqueta.setBounds(10, 10,30, 30);      // ubicación de la etiqueta
 
         etiqueta.setForeground(Color.ORANGE);           // Cambiar color de la letra
         etiqueta.setOpaque(true);
         etiqueta.setBackground(Color.WHITE);            // Cambiar fondo del cuadro del JLabel
         panel.add(etiqueta);                            // Agregar etiqueta al panel
 
+        JLabel etiqueta1 = new JLabel("I'm Juan", SwingConstants.LEADING);
+        etiqueta1.setBounds(50, 50, 70, 20);
+        etiqueta1.setForeground(Color.GREEN);
+        etiqueta1.setOpaque(true);
+        etiqueta1.setBackground(Color.DARK_GRAY);
+        panel.add(etiqueta1);
 
+        JLabel etiqueta2 = new JLabel();
+        etiqueta2.setText("Camilo Galvis");
+        etiqueta2.setHorizontalAlignment(SwingConstants.CENTER);
+        etiqueta2.setBounds(200, 100, 100, 20);
+        etiqueta2.setForeground(Color.BLUE);
+        etiqueta2.setOpaque(true);
+        etiqueta2.setBackground(Color.DARK_GRAY);
+        panel.add(etiqueta2);
+
+        JLabel etiqueta3 = new JLabel();
+        etiqueta3.setText("Mafe chan");
+        etiqueta3.setHorizontalAlignment(SwingConstants.LEFT);
+        etiqueta3.setBounds(200, 400, 200, 100);
+        etiqueta3.setForeground(Color.BLUE);
+        etiqueta3.setOpaque(true);
+        etiqueta3.setBackground(Color.LIGHT_GRAY);
+        etiqueta3.setFont(new Font("Calibri", Font.ITALIC, 30));
+        panel.add(etiqueta3);
     }
 
 }
